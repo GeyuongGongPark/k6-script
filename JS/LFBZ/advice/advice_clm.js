cabe94969aaee6b3f0b7db2133380bf8ee33a8e5
Gyeonggong Park <kar_asirian@naver.com>
2025-06-28 00:12:20 +0900

    advice_draft.js add

---
+++ JS/LFBZ/advice/advice_clm.js
@@ -0,0 +1,52 @@
+import { check } from "k6";
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { browser } from "k6/browser";
+import login_to_dashborad from "../login/login_to_dashborad";
+
+function getFormattedTimestamp() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const day = String(now.getDay() + 1).padStart(2, '0');
+    const hours = String(now.getHours() + 1).padStart(2, '0');
+    const minutes = String(now.getMinutes() + 1).padStart(2, '0');
+    const seconds = String(now.getSeconds() + 1).padStart(2, '0');
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
+export const options = {
+    scenarios: {
+        ui: {
+            executor: 'shared-iterations',
+            options: {
+                browser: {
+                    type: 'chromium',
+                },
+            },
+        },
+    },
+    thresholds: {
+        checks: ['rate==1.0'],
+    }
+}
+
+export default async function () {
+    const timestamp = getFormattedTimestamp().replace(/:/g, '_ ');
+
+    try {
+        const page = await login_to_dashborad();
+        await page.goto('https://business.lawform.io/advice/draft');
+        await page.screenshot({path: `screenshots/screenshots_${timestamp}.png`});
+        return page;
+    }
+    finally {
+        await page.close();
+    }
+}
+
+export function handleSummary(data) {
+    const timestamp = getFormattedTimestamp().replace(/:/g, '_');
+    return {
+        [`Reuslt/advice_draft_summary_${timestamp}.html`]: htmlReport(data),
+    };
+}
